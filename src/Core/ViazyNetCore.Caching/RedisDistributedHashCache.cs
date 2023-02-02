0b8caa4fa5ca355b4df944eea5efb7670f549f75
林贤炜 <542114415@qq.com>
2023-02-02 18:59:08 +0800

    1.调整redis

---
+++ src/Core/ViazyNetCore.Caching/RedisDistributedHashCache.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Caching.StackExchangeRedis;
+using Microsoft.Extensions.Options;
+using ViazyNetCore.Redis;
+
+namespace ViazyNetCore.Caching
+{
+    public class RedisDistributedHashCache : RedisCache, IDistributedHashCache
+    {
+        public RedisDistributedHashCache(IOptions<RedisCacheOptions> optionsAccessor) : base(optionsAccessor)
+        {
+        }
+    }
+}
