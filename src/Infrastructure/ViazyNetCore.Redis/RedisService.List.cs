74031a4508380ebc27f48e44960a6111899ad005
林贤炜 <542114415@qq.com>
2023-02-02 18:27:38 +0800

    1.调整redis 结构

--- src/Infrastructure/ViazyNetCore.Redis/RedisService.List.cs
+++ src/Infrastructure/ViazyNetCore.Redis/RedisService.List.cs
@@ -11,19 +11,182 @@ namespace ViazyNetCore.Redis
 {
     public partial class RedisService
     {
-        public List<T?> GetAllItemsFromList<T>(string key, int? db = null)
+        /// <summary>
+        /// 根据key获取RedisValue
+        /// </summary>
+        /// <param name="redisKey"></param>
+        /// <returns></returns>
+        public async Task<RedisValue[]> ListRangeAsync(string redisKey)
         {
-            return this._database.HashGetAll(key).Select(p => JSON.Parse<T>(p.Value!)).ToList();
+            return await _database.ListRangeAsync(redisKey);
         }
 
-        public List<T?> GetRangeFromList<T>(string key, int startingFrom, int endingAt, int? db = null)
+        public List<T?> ListRangeAsync<T>(string redisKey)
         {
-            return this._database.ListRange(key, startingFrom, endingAt).Select(p => JSON.Parse<T>(p!)).ToList();
+            return this._database.ListRange(redisKey).Select(p => JSON.Parse<T>(p!)).ToList();
         }
 
-        public void AddItemToList<T>(string listId, T value, int? db = null)
+        public List<T?> GetRangeFromList<T>(string redisKey, int startingFrom, int endingAt, int? db = null)
         {
-            this._database.SetAdd(listId, JSON.Stringify(value));
+            return this._database.ListRange(redisKey, startingFrom, endingAt).Select(p => JSON.Parse<T>(p!)).ToList();
+        }
+
+        public void AddItemToList<T>(string redisKey, T value, int? db = null)
+        {
+            this._database.SetAdd(redisKey, JSON.Stringify(value));
+        }
+
+
+        /// <summary>
+        /// 在列表头部插入值。如果键不存在，先创建再插入值
+        /// </summary>
+        /// <param name="redisKey"></param>
+        /// <param name="redisValue"></param>
+        /// <returns></returns>
+        public async Task<long> ListLeftPushAsync(string redisKey, string redisValue)
+        {
+            return await _database.ListLeftPushAsync(redisKey, redisValue);
+        }
+
+        /// <summary>
+        /// 在列表尾部插入值。如果键不存在，先创建再插入值
+        /// </summary>
+        /// <param name="redisKey"></param>
+        /// <param name="redisValue"></param>
+        /// <returns></returns>
+        public async Task<long> ListRightPushAsync(string redisKey, string redisValue)
+        {
+            return await _database.ListRightPushAsync(redisKey, redisValue);
+        }
+
+        /// <summary>
+        /// 在列表尾部插入数组集合。如果键不存在，先创建再插入值
+        /// </summary>
+        /// <param name="redisKey"></param>
+        /// <param name="redisValue"></param>
+        /// <returns></returns>
+        public async Task<long> ListRightPushAsync(string redisKey, IEnumerable<string> redisValue)
+        {
+            var redislist = new List<RedisValue>();
+            foreach (var item in redisValue)
+            {
+                redislist.Add(item);
+            }
+            return await _database.ListRightPushAsync(redisKey, redislist.ToArray());
+        }
+
+        /// <summary>
+        /// 移除并返回存储在该键列表的第一个元素  反序列化
+        /// </summary>
+        /// <param name="redisKey"></param>
+        /// <returns></returns>
+        public async Task<T?> ListLeftPopAsync<T>(string redisKey) where T : class
+        {
+            var cacheValue = await _database.ListLeftPopAsync(redisKey);
+            if (string.IsNullOrEmpty(cacheValue)) return null;
+            var res = JsonConvert.DeserializeObject<T>(cacheValue!);
+            return res;
+        }
+
+        /// <summary>
+        /// 移除并返回存储在该键列表的最后一个元素   反序列化
+        /// 只能是对象集合
+        /// </summary>
+        /// <param name="redisKey"></param>
+        /// <returns></returns>
+        public async Task<T?> ListRightPopAsync<T>(string redisKey) where T : class
+        {
+            var cacheValue = await _database.ListRightPopAsync(redisKey);
+            if (string.IsNullOrEmpty(cacheValue)) return null;
+            var res = JsonConvert.DeserializeObject<T>(cacheValue!);
+            return res;
+        }
+
+        /// <summary>
+        /// 移除并返回存储在该键列表的第一个元素   
+        /// </summary>
+        /// <param name="redisKey"></param>
+        /// <returns></returns>
+        public async Task<string?> ListLeftPopAsync(string redisKey)
+        {
+            return await _database.ListLeftPopAsync(redisKey);
+        }
+
+        /// <summary>
+        /// 移除并返回存储在该键列表的最后一个元素   
+        /// </summary>
+        /// <param name="redisKey"></param>
+        /// <returns></returns>
+        public async Task<string?> ListRightPopAsync(string redisKey)
+        {
+            return await _database.ListRightPopAsync(redisKey);
+        }
+
+        /// <summary>
+        /// 列表长度
+        /// </summary>
+        /// <param name="redisKey"></param>
+        /// <returns></returns>
+        public async Task<long> ListLengthAsync(string redisKey)
+        {
+            return await _database.ListLengthAsync(redisKey);
+        }
+
+        /// <summary>
+        /// 返回在该列表上键所对应的元素
+        /// </summary>
+        /// <param name="redisKey"></param>
+        /// <returns></returns>
+        public async Task<IEnumerable<string>> ListRangeAsync(string redisKey, int db = -1)
+        {
+            var result = await _database.ListRangeAsync(redisKey);
+            return result.Select(o => o.ToString());
+        }
+
+        /// <summary>
+        /// 根据索引获取指定位置数据
+        /// </summary>
+        /// <param name="redisKey"></param>
+        /// <param name="start"></param>
+        /// <param name="stop"></param>
+        /// <returns></returns>
+        public async Task<IEnumerable<string>> ListRangeAsync(string redisKey, int start, int stop)
+        {
+            var result = await _database.ListRangeAsync(redisKey, start, stop);
+            return result.Select(o => o.ToString());
+        }
+
+        /// <summary>
+        /// 删除List中的元素 并返回删除的个数
+        /// </summary>
+        /// <param name="redisKey">key</param>
+        /// <param name="redisValue">元素</param>
+        /// <param name="type">大于零 : 从表头开始向表尾搜索，小于零 : 从表尾开始向表头搜索，等于零：移除表中所有与 VALUE 相等的值</param>
+        /// <returns></returns>
+        public async Task<long> ListDelRangeAsync(string redisKey, string redisValue, long type = 0)
+        {
+            return await _database.ListRemoveAsync(redisKey, redisValue, type);
+        }
+
+        /// <summary>
+        /// 清空List
+        /// </summary>
+        /// <param name="redisKey"></param>
+        public async Task ListClearAsync(string redisKey)
+        {
+            await _database.ListTrimAsync(redisKey, 1, 0);
+        }
+
+
+        /// <summary>
+        /// 有序集合/定时任务延迟队列用的多
+        /// </summary>
+        /// <param name="redisKey">key</param>
+        /// <param name="redisValue">元素</param>
+        /// <param name="score">分数</param>
+        public async Task SortedSetAddAsync(string redisKey, string redisValue, double score)
+        {
+            await _database.SortedSetAddAsync(redisKey, redisValue, score);
         }
     }
 }
