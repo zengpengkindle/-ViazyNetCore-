70f0a296725f078d94583b349337c31689e9a026
林贤炜 <542114415@qq.com>
2023-07-24 19:03:07 +0800

    添加字段可变字段

---
+++ modules/ViazyNetCore.TunnelWorks/src/ViazyNetCore.TunnelWorks.Models/CrmFieldStyle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViazyNetCore.TunnelWorks.Models
+{
+    public class CrmFieldStyle : EntityUpdate
+    {
+        public string Style { get; set; }
+        public int Type { get; set; }
+        public string FieldName { get; set; }
+    }
+}
