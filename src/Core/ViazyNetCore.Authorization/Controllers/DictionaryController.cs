233f0014c1b13a6504bb5b21c8f917477679af43
林贤炜 <542114415@qq.com>
2023-08-28 18:46:08 +0800

    调整字典获取

--- src/Core/ViazyNetCore.Authorization/Controllers/DictionaryController.cs
+++ src/Core/ViazyNetCore.Authorization/Controllers/DictionaryController.cs
@@ -3,18 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using ViazyNetCore.Authorization.Modules;
+using ViazyNetCore.Authorization.ViewModels;
 
 namespace ViazyNetCore.Authorization
 {
     public class DictionaryController : DynamicControllerBase
     {
         private readonly IDictionaryService _dictionaryService;
+        private readonly IMapper _mapper;
 
-        public DictionaryController(IDictionaryService dictionaryService)
+        public DictionaryController(IDictionaryService dictionaryService, IMapper mapper)
         {
             this._dictionaryService = dictionaryService;
+            this._mapper = mapper;
         }
 
         [HttpPost]
@@ -71,9 +75,10 @@ namespace ViazyNetCore.Authorization
         [HttpPost]
         [Permission(PermissionIds.Setting)]
         [Route("getvalues")]
-        public async Task<List<DictionaryValue>> FindAllValuesAsync(string code)
+        public async Task<List<DictionaryValueViewResult>> FindAllValuesAsync(string code)
         {
-            return await this._dictionaryService.GetAllValuesAsync(code);
+            var result = await this._dictionaryService.GetAllValuesAsync(code);
+            return this._mapper.Map<List<DictionaryValue>, List<DictionaryValueViewResult>>(result);
         }
 
         [HttpPost]
---
+++ src/Core/ViazyNetCore.Authorization/Mapper/DictionaryProfile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using ViazyNetCore.Authorization.ViewModels;
+
+namespace ViazyNetCore.Authorization.Mapper
+{
+    public class DictionaryProfile : Profile
+    {
+        public DictionaryProfile()
+        {
+            CreateMap<DictionaryValue, DictionaryValueViewResult>();
+        }
+    }
+}
