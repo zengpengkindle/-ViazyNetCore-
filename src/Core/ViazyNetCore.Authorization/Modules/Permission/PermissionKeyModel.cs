744e8023c44020ff2274fe87db62752614640c8d
viazy <542114415@qq.com>
2023-09-18 20:26:51 +0800

    修复类型错误

--- src/Core/ViazyNetCore.Authorization/Modules/Permission/PermissionKeyModel.cs
+++ src/Core/ViazyNetCore.Authorization/Modules/Permission/PermissionKeyModel.cs
@@ -9,7 +9,7 @@ namespace ViazyNetCore.Authorization.Modules
     public class PermissionKey
     {
         public string Key { get; set; }
-        public int Id { get; set; }
+        public long Id { get; set; }
         public string Name { get; set; }
     }
 
