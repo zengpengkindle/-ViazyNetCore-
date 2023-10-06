f436b43dcb1acaf566a53c5be6decc7a98f1455a
viazy <542114415@qq.com>
2023-10-06 19:20:35 +0800

    除杂

--- src/Core/ViazyNetCore.Authorization/DataSeed/PrimssionKeyDataSeed.cs
+++ src/Core/ViazyNetCore.Authorization/DataSeed/PrimssionKeyDataSeed.cs
@@ -14,21 +14,16 @@ namespace ViazyNetCore.Authorization
     public class PrimssionKeyDataSeed
     {
         private readonly IApiManager _apiManager;
-        private readonly IFreeSql _freeSql;
-        private readonly IOptions<DbConfig> _options;
         private readonly PermissionService _permissionService;
 
-        public PrimssionKeyDataSeed(IApiManager apiManager, IFreeSql freeSql, IOptions<DbConfig> options, PermissionService permissionService)
+        public PrimssionKeyDataSeed(IApiManager apiManager, PermissionService permissionService)
         {
             this._apiManager = apiManager;
-            this._freeSql = freeSql;
-            this._options = options;
             this._permissionService = permissionService;
         }
 
         public async Task CreatePrimissionKeyAsync()
         {
-            var fsql = this._freeSql.UseDb(this._options.Value.Key);
             foreach (var primssion in this._apiManager.GetPermissionKeys())
             {
                 await _permissionService.AddPermission(primssion, primssion, true);
